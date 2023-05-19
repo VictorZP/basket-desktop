6c87f9752f3309a6190ff018cdeecf2a2bf526f8
Yevhen Bakhtizin <eugenbah@gmail.com>
2023-05-19 03:45:18 +0300

    add dependencies

--- forge.config.js
+++ forge.config.js
@@ -23,6 +23,7 @@ module.exports = {
 		{
 			name: "@electron-forge/plugin-webpack",
 			config: {
+				port: 3000,
 				mainConfig: "./webpack.main.config.js",
 				devContentSecurityPolicy: `default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;`,
 				renderer: {
