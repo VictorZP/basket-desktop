f457f4784bf7005239ac8e22ea5835f62b67d56f
Yevhen Bakhtizin <eugenbah@gmail.com>
2023-06-08 17:29:30 +0300

    feature/url add initial section

--- src/renderer/pages/Matches.jsx
+++ src/renderer/pages/Matches.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 
-import Typography from "@mui/material/Typography";
+import { Box, Typography, Button } from "@mui/material";
+
+import LoadingSpinner from "../components/LoadingSpinner";
+
+import { CONSTANTS } from "../constants/matchesPage.js";
 
 const Matches = () => {
-	return <Typography paragraph></Typography>;
+	return (
+		<Box sx={{ outline: "1px solid red" }}>
+			<Box>
+				<Typography variant="h5">{CONSTANTS.ADD_URL.TITLE}</Typography>
+				<Button variant="outlined" size="small">
+					{CONSTANTS.ADD_URL.BTN_OPEN}
+				</Button>
+				<Suspense fallback={<LoadingSpinner height={"calc(100vh - 128px)"} />}>
+					<Outlet />
+				</Suspense>
+			</Box>
+		</Box>
+	);
 };
 
 export default Matches;
