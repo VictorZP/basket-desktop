9773f07ec717505084e5a45405e4930eb73350e9
culozik <eugenbah@gmail.com>
2024-02-28 19:25:41 +0200

    fix: fixed  state value name

--- src/renderer/redux/teamTransfer/teamTransferSlice.js
+++ src/renderer/redux/teamTransfer/teamTransferSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
 	outCyberId: "",
-	outChapId: "",
+	outChampId: "",
 	targetCyberId: "",
 	targetChampId: "",
 	teamsIds: [],
