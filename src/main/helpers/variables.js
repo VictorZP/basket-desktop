6d8a0fd18594548cc1f525686f3e75a684b636e3
culozik <eugenbah@gmail.com>
2024-04-09 17:59:30 +0300

    upd: change api addresses

--- src/main/helpers/variables.js
+++ src/main/helpers/variables.js
@@ -1,7 +1,28 @@
-const ADDRESS = "45.134.254.87";
-const WS_ADDRESS = "45.134.254.87";
-const PORT = "8000";
-const WS_PORT = "8010";
+const fs = require("fs");
+const path = require("path");
+
+const mode = process.env.NODE_ENV;
+
+let config;
+
+if (mode !== "production") {
+	config = {
+		MAIN_ADDRESS: "localhost",
+		MAIN_WS_PARSER_ADDRESS: "localhost",
+		PORT: "8000",
+		WS_PARSER_PORT: "8010",
+	};
+} else {
+	const rawConfigData = fs.readFileSync(
+		path.join(__dirname, "./data/config.json")
+	);
+	config = JSON.parse(rawConfigData);
+}
+
+const ADDRESS = config.MAIN_ADDRESS;
+const WS_ADDRESS = config.MAIN_WS_PARSER_ADDRESS;
+const PORT = config.PORT;
+const WS_PORT = config.WS_PARSER_PORT;
 
 module.exports = {
 	ADDRESS,
