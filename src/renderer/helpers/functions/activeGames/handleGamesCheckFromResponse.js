1eec7afae199715c97b12a380fa61c298b3591b4
culozik <eugenbah@gmail.com>
2024-05-26 22:56:01 +0300

    upd: improve active games list logic for ended matches

--- src/renderer/helpers/functions/activeGames/handleGamesCheckFromResponse.js
+++ src/renderer/helpers/functions/activeGames/handleGamesCheckFromResponse.js
@@ -11,13 +11,21 @@ const handleGamesCheckFromResponse = (
 	const matchesData = [...matches];
 
 	const ndx = matchesData.findIndex(
-		(match) => match?.Id === gameFromResponse?.id
+		(match) => match?.matchId === gameFromResponse?.matchId
 	);
 
 	// Checking for the presence of a match in the array, as well as checking the status of the match (whether to display it or not)
 	if (ndx < 0) {
 		matchesData.push(gameFromResponse);
+		setMatches(matchesData);
 		ipcRenderer.send(CHANNELS.ANALYZE.SHOW_NOTIFICATION, gameFromResponse); // Sends a notification when a bid first appears
+		return;
+	}
+
+	if (matches[ndx]?.status === ACTIVE_PAGE.MOMENT_CHECKED) {
+		matchesData.splice(ndx, 1);
+		setMatches(matchesData);
+		return;
 	}
 
 	if (matches[ndx]?.statusFront === ACTIVE_PAGE.STATUS) {
@@ -26,9 +34,8 @@ const handleGamesCheckFromResponse = (
 
 	if (matches[ndx]?.statusFront !== ACTIVE_PAGE.STATUS) {
 		matchesData.splice(ndx, 1, gameFromResponse);
+		setMatches(matchesData);
 	}
-
-	setMatches(matchesData);
 };
 
 export default handleGamesCheckFromResponse;
